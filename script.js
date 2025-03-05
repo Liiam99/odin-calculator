aa06a123dc65188f93cb53b44f1dab2dffc40231
Liam <liam99mail@proton.me>
2025-03-05 13:24:31 +0000

    Dynamically operate based on the given operator

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
+(() => {
+    const leftOperand = null;
+    const operator = null;
+    const rightOperand = null;
+})();
+
+
+function operate(leftOperand, operator, rightOperand) {
+    let operation;
+
+    switch(operator) {
+        case '+':
+            operation = add;
+            break;
+
+        case '-':
+            operation = subtract;
+            break;
+
+        case '*':
+            operation = multiply;
+            break;
+
+        case '/':
+            operation = divide;
+            break;
+
+        default:
+            throw new Error('Incorrect or unsupported operator.')
+    }
+
+    return operation(leftOperand, rightOperand);
+}
+
+
 function add(a, b) {
     return a + b;
 }
+
 
 function subtract(a, b) {
     return a - b;
 }
 
+
 function multiply(a, b) {
     return a*b;
 }
+
 
 function divide(a, b) {
     return a/b;
